1f60e95d516ffcd6a1fc60306139179cfa978841
Daniel Modrzejewski <daniel.modrzejewski@accenture.com>
2023-03-16 14:22:26 +0100

    init project

---
+++ src/main/java/com/example/LoggerApp/LoggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoggerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoggerApp/LoggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoggerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
